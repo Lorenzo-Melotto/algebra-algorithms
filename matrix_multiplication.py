249182acd36ef75f464dcd7b3c49e040f14eb720
Lorenzo Melotto <8791533+Lorenzo-Melotto@users.noreply.github.com>
2023-12-07 14:40:16 +0100

    fixed bug with matrices of different sizes

--- matrix_multiplication.py
+++ matrix_multiplication.py
@@ -17,32 +17,46 @@ def matrix_multiplication(m1: list[list[float]], m2: list[list[float]]) -> list[
     rows = len(m1)
     cols = len(m2[0])
     res = [[0.0 for _ in range(cols)] for _ in range(rows)]
-    for i in range(rows):
-        for j in range(cols):
+    for r in range(rows):
+        for c in range(cols):
             temp = 0.0
-            for k in range(rows):
-                temp += m1[i][k] * m2[k][j]
-            res[i][j] = temp
+            for cs in range(len(m1[0])):
+                temp += m1[r][cs] * m2[cs][c]
+            res[r][c] = temp
     return res
 
+def print_matrix(m: list[list[float]]) -> None:
+    for i in range(len(m)):
+        for j in range(len(m[0])):
+            print(m[i][j], end="\t")
+        print()
+
 def main():
+    
     # matrix 1
-    d1 = read_int("Number of rows of first matrix: ", min=1)
-    d2 = read_int("Number of columns of first matrix: ", min=1)
-    m1 = load_matrix(d1, d2)
+    d1: int = read_int("Number of rows of first matrix: ", min=1)
+    d2: int = read_int("Number of columns of first matrix: ", min=1)
+    m1: list[list[float]] = load_matrix(d1, d2)
     
     # matrix 2
     d1 = read_int("Number of rows of second matrix: ", min=1)
     d2 = read_int("Number of columns of second matrix: ", min=1)
-    m2 = load_matrix(d1, d2)
+    m2: list[list[float]] = load_matrix(d1, d2)
+    
+    print("Matrix 1: ")
+    print_matrix(m1)
+    
+    print()
+    
+    print("Matrix 2:")
+    print_matrix(m2)
 
-    res = matrix_multiplication(m1, m2)
+    print()
+
+    res: list[list[float]] = matrix_multiplication(m1, m2)
 
     # print the result
-    for i in range(len(res)):
-        for j in range(len(res[0])):
-            print(res[i][j], end="\t")
-        print()
+    print_matrix(res)
 
 if __name__ == "__main__":
     main()
